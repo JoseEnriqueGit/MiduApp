ed89d893f89c6fc94341bcf68fc41596b12ebe41
Enrique.Git <75050597+JoseEnriqueGit@users.noreply.github.com>
2023-04-29 22:47:12 -0400

    Model

---
+++ BackEnd/src/models/noteModel.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const noteSchema = new mongoose.Schema({
+	description: {
+		type: String,
+		trim: false,
+	},
+	isImportant: {
+		type: Boolean
+	}
+});
+
+export default mongoose.model("Notes", noteSchema);
