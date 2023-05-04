0fcf7642391cb7be95f90548a19affca5ba3fda8
Enrique.Git <75050597+JoseEnriqueGit@users.noreply.github.com>
2023-05-03 22:16:03 -0400

    Cors update

--- BackEnd/src/server.js
+++ BackEnd/src/server.js
@@ -5,10 +5,17 @@ import Routers from './routes/noteRouters.js';
 const server = express();
 
 // middlewares
-server.use(cors(), express.json());
+// server.use(cors(), express.json());
+server.use(cors({
+  origin: 'http://localhost:3000',
+  optionsSuccessStatus: 200
+}), express.json());
 
 // Routes
 server.use(Routers);
+server.use((req, res) => {
+  res.status(404).json({ message: 'Ruta no encontrada' });
+});
 
 const PORT = process.env.PORT || 4000;
 
